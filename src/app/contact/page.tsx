df7b3b5ab8e03a177c77bdb108d43264723f54ac
Nicolas Devaux <ndevaux@gmail.com>
2024-11-25 19:32:20 +0000

    .

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -59,64 +59,6 @@ const ContactPage = () => {
             bien-être.
           </p>
         </section>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          className="flex flex-col gap-5 bg-sazerac-50 p-10 shadow-md"
-        >
-          <p>
-            <label className="flex flex-col gap-1">
-              Nom{' '}
-              <input
-                type="text"
-                name="name"
-                placeholder="Jean Dupond"
-                className="flex-1 rounded border p-1"
-              />
-            </label>
-          </p>
-          <p>
-            <label className="flex flex-col gap-1">
-              Email{' '}
-              <input
-                type="email"
-                name="email"
-                placeholder="vous@domain.com"
-                className="flex-1 rounded border p-1"
-              />
-            </label>
-          </p>
-          <p>
-            <label className="flex flex-col gap-1">
-              Téléphone{' '}
-              <input
-                type="tel"
-                name="phone"
-                placeholder="xx xx xx xx"
-                className="flex-1 rounded border p-1"
-              />
-            </label>
-          </p>
-          <p>
-            <label className="flex flex-col gap-1">
-              Votre message{' '}
-              <textarea
-                placeholder="votre message"
-                className="flex-1 rounded border p-1"
-                rows={6}
-              />
-            </label>
-          </p>
-          <p>
-            <button
-              type="submit"
-              className="c-btn-cta"
-            >
-              Envoyer
-            </button>
-          </p>
-        </form>
       </div>
     </section>
   )
